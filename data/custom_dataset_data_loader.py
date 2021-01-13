14edfd27eb40e50659e0ac7a079da0e03de5ff44
tyunist <tynguyen@unist.ac.kr>
2021-01-13 12:01:49 -0500

    Disable resize_or_crop in generating test images

--- data/custom_dataset_data_loader.py
+++ data/custom_dataset_data_loader.py
@@ -1,6 +1,6 @@
 import torch.utils.data
 from data.base_data_loader import BaseDataLoader
-
+import pdb
 
 def CreateDataset(opt):
     dataset = None
--- scripts/gen_three_drones_syn_data.sh
+++ scripts/gen_three_drones_syn_data.sh
@@ -1,10 +1,11 @@
-data_name="fake_imgs/256x256_autel"
-split_name="test"
+data_name="fake_imgs/256x256_fla_450"
+split_name="train"
 data_path="./datasets/${data_name}"
 results_dir="datasets"
 python3.7 gen_syn_data.py\
    --dataroot ${data_path}\
    --name ${data_name}\
+   --resize_or_crop None\
    --model test\
    --phase ${split_name}\
    --results_dir ${results_dir}\
